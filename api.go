8e6110ce677b75cb893e8fd486d66415436a1269
brutatoasta <60847718+brutatoasta@users.noreply.github.com>
2023-09-14 23:36:14 +0800

    feat: add api

---
+++ api.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"fmt"
+)
+
+
+func create(path string, data[], flags[]) string{
+
+}
+
+func delete(path string, version int64){
+
+}
+
+func exists(path string, watch bool) bool{
+
+}
+
+func getData(path string, watch bool){
+
+}
+
+func setData(path string, data[], version int64){
+	
+}
+
+func getChildren(path string, watch bool){
+
+}
+
+func sync(path string){
+
+}
