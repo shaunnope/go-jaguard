9ef550ae93dc52e477d509adbebd3ba41b7f4bd7
shaunnope <seanyap123@gmail.com>
2023-09-19 22:56:29 +0800

    feat: zouk protoc add shell script to build pb files

---
+++ build.sh
@@ -0,0 +1,5 @@
+export PATH="$PATH:$(go env GOPATH)/bin"
+
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    zouk/zouk.proto
--- server/server.go
+++ server/server.go
@@ -1,11 +1,25 @@
-package main 
+package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net"
+
+	"google.golang.org/grpc"
+)
+
+var (
+	// flags
+	port = flag.Int("port", 50051, "server port")
 )
 
 func main() {
-	// listener, error?
-	lis, err := net.Listen()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
 }
