02e671d4229a0008d759ac2eeac2139c61f780af
shaunnope <seanyap123@gmail.com>
2023-09-20 13:44:34 +0800

    feat: add some stuff

--- server/server.go
+++ server/server.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 
+	pb "github.com/shaunnope/go-jaguard/zouk"
 	"google.golang.org/grpc"
 )
 
@@ -14,12 +16,25 @@ var (
 	port = flag.Int("port", 50051, "server port")
 )
 
+type server struct {
+	pb.UnimplementedNodeServer
+}
+
+func (s *server) Create(ctx context.Context, in *pb.CreateRequest) (*pb.CreateResponse, error) {
+	return &pb.CreateResponse{}, nil
+}
+
 func main() {
+	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
 	s := grpc.NewServer()
+	pb.RegisterNodeServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 
 }
