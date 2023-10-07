feaf522a2fc9083a7593e118560170c60e8d9167
Joshua Ng <joshuantw@gmail.com>
2023-10-07 18:22:59 +0800

    create using grpc

--- server/server.go
+++ server/server.go
@@ -21,55 +21,24 @@ type server struct {
 	pb.UnimplementedNodeServer
 }
 
-// request
-func (s *server) CreateRequest_Client(ctx context.Context, in *pb.CreateRequest_Client) (*pb.CreateResponse_Client, error) {
-	// if leader, send proposal
-	// if follower, forward to leader using pb.RequestType_CLIENT
-	return &pb.CreateResponse_Client{}, nil
-}
-
-func (s *server) CreateRequest_Proposal(ctx context.Context, in *pb.CreateRequest_Proposal) (*pb.CreateResponse_Announcement, error) {
-	// leaders dont get announcements, panic
-	// followers commit locally
-	return &pb.CreateResponse_Announcement{}, nil
-}
-
-func (s *server) CreateRequest_Announcement(ctx context.Context, in *pb.CreateRequest_Announcement) (*pb.CreateResponse_Proposal, error) {
-	// if leader, send announcement
-	// if follower reply proposal
-	return &pb.CreateResponse_Proposal{}, nil
-}
-
-// response
-func (s *server) CreateResponse_Client(ctx context.Context, in *pb.CreateResponse_Client) error {
-
-}
-
-func (s *server) CreateResponse_Announcement(ctx context.Context, in *pb.CreateResponse_Announcement) (*pb.CreateResponse_Announcement, error) {
-	// leaders dont get announcements, panic
-	// followers commit locally
-	return &pb.CreateResponse_Announcement{}, nil
-}
-
-func (s *server) CreateResponse_Proposal(ctx context.Context, in *pb.CreateResponse_Proposal) (*pb.CreateResponse_Proposal, error) {
-	// if leader, send announcement
-	// if follower reply proposal
-	return &pb.CreateResponse_Proposal{}, nil
-}
-
-func (s *server) CreateResponse(ctx context.Context, in *pb.CreateResponse) {
-	// Handle CreateResponse
-	switch in.ResponseType {
-	case pb.ResponseType_PROPOSAL:
-		// if leader, collate proposals, if enough and commit, send announcement and commit locally
-		// followers dont get acknowledgement, panic
+func (s *server) Create(ctx context.Context, in *pb.CreateRequest) (*pb.CreateResponse, error) {
+	// Handle incoming CreateRequest
+	switch in.RequestType {
+	case pb.RequestType_PROPOSAL:
+		// if leader, send announcement, do nothing with response (rpc)
+		// if follower send proposal reply, do nothing with response (rpc)
 	case pb.RequestType_ANNOUNCEMENT:
-		// if leader, do nothing ( no need to respond to annoucement acknowledgements)
-		// if follower
-
+		// leaders dont get announcements, panic
+		// followers commit locally
+	case pb.RequestType_CLIENT:
+		// if leader send proposal to all followers in for loop (rpc)
+		// since its rpc, leader will monitor for responses and decide whether to commit/announce
+		// if follower forward to leader, do nothing with response (rpc)
 	}
+	return &pb.CreateResponse{}, nil
 
 }
+
 func main() {
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
