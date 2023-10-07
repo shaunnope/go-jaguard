ff3dbad247290a26b71790a34806044ff860bbc8
Joshua Ng <joshuantw@gmail.com>
2023-10-07 18:42:38 +0800

    update client

--- client/client_main.go
+++ client/client_main.go
@@ -1,13 +1,21 @@
 package client
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log"
+	"time"
+
+	pb "github.com/shaunnope/go-jaguard/zouk"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
 	// flags
 	port = flag.Int("port", 50051, "server port")
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
 )
 
 func main() {
@@ -15,4 +23,21 @@ func main() {
 	// goroutines
 	flag.Parse()
 	fmt.Printf("%v", port)
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewNodeClient()
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Create(ctx, &pb.CreateRequest{Path: "/home/folder1", Data: [][]byte{50}, Flags: "flag", RequestType: pb.RequestType_CLIENT})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %b", r.Accept)
 }
